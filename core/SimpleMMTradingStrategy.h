369ffcacab8a9d8de6197983efc9626885a9e082
Richard Chan <richardchankiyin@gmail.com>
2023-10-14 16:02:20 +0800

    add more implementation to SimpleMMTradingStrategy

--- core/SimpleMMTradingStrategy.h
+++ core/SimpleMMTradingStrategy.h
@@ -17,19 +17,45 @@ private:
      string _ccy;
      double _initcashbalance;
      double _initinstrumentbalance;
+     double _securitysecmarketbestbid;
+     double _securitysecmarketbestask;
+     double _securitysecmarketbestbidqty;
+     double _securitysecmarketbestaskqty;
+
      PositionKeeper* _pk;  
 
 public:
-     SimpleMMTradingStrategy(string ccy, double initcashbalance, string securityid, double initinstrumentbalance) {
+     SimpleMMTradingStrategy(string ccy, double initcashbalance, string securityid, double initinstrumentbalance, double securitysecmarketbestbid, double securitysecmarketbestask, double securitysecmarketbestbidqty, double securitysecmarketbestaskqty) {
         _ccy=ccy;
 	_securityid=securityid;
 	_initcashbalance=initcashbalance;
 	_initinstrumentbalance=initinstrumentbalance;
+        _securitysecmarketbestbid=securitysecmarketbestbid;
+        _securitysecmarketbestask=securitysecmarketbestask;
+        _securitysecmarketbestbidqty=securitysecmarketbestbidqty;
+        _securitysecmarketbestaskqty=securitysecmarketbestaskqty;
 	_pk = new PositionKeeper(_ccy,_initcashbalance,_securityid,_initinstrumentbalance);
      }
      ~SimpleMMTradingStrategy() {
         delete _pk;
      }
+     void setsecmarket(double securitysecmarketbestbid, double securitysecmarketbestask, double securitysecmarketbestbidqty, double securitysecmarketbestaskqty) {
+        _securitysecmarketbestbid=securitysecmarketbestbid;
+        _securitysecmarketbestask=securitysecmarketbestask;
+        _securitysecmarketbestbidqty=securitysecmarketbestbidqty;
+        _securitysecmarketbestaskqty=securitysecmarketbestaskqty;
+     }
+     string ccy() { return _ccy; }
+     string securityid() { return _securityid; }
+     double initcashbalance() { return _initcashbalance; }
+     double initinstrumentbalance() { return _initinstrumentbalance; }
+     double securitysecmarketbestbid() { return _securitysecmarketbestbid; }
+     double securitysecmarketbestask() { return _securitysecmarketbestask; }
+     double securitysecmarketbestbidqty() { return _securitysecmarketbestbidqty; }
+     double securitysecmarketbestaskqty() { return _securitysecmarketbestaskqty; }
+     double cashbalance() { return _pk->cashbalance(); }
+     double instrumentbalance() { return _pk->instrumentbalance(); }
+
      void onOrderAdd(OrderBook& orderBook, const OrderInfo& orderInfo) {
         //TODO
 
